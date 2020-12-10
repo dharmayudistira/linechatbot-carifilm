921ec96aa5a0b0244f05c5d068978ecba1f7e973
dharmayudistira <dharmayudistira2000@gmail.com>
2020-12-10 12:09:46 +0700

    erere

--- src/main/java/com/pandeka/CariFilm/controller/BotController.java
+++ src/main/java/com/pandeka/CariFilm/controller/BotController.java
@@ -197,7 +197,7 @@ public class BotController {
             ObjectMapper objectMapper = new ObjectMapper();
             movies = objectMapper.readValue(jsonResponse, Movies.class);
 
-            System.out.println("Movies: " + movies.toString());
+            System.out.println("Movies: " + movies.getResults().toString());
         }catch (InterruptedException | ExecutionException | IOException e) {
             throw new RuntimeException(e);
         }
